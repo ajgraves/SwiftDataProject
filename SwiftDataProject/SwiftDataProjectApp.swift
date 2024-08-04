6d0d5d8f6d4df8929740477b11a121c661fea16e
Aaron Graves <ajgraves@ajgraves.com>
2024-08-03 22:42:27 -0500

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Aaron Graves on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
